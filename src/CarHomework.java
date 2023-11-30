a7c8b464af9df161c2595ffe274063c709b105c9
marktimestwo <markescabarte79@yahoo.com>
2023-11-30 22:36:37 +0000

    double running

--- src/CarHomework.java
+++ src/CarHomework.java
@@ -16,5 +16,6 @@ public class CarHomework {
     public void drive(double direction) {
         System.out.printf("The %s %s is driving at %s at %d degrees", colour, make, direction);
         System.out.printf("The %s %s is not driving at %s at %d degrees", colour, make, direction);
+        System.out.printf("The %s %s is running at %s at %d degrees", colour, make, direction);
     }
 }
